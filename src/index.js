61adce4549e09a3aed19585876e9a0fd4c1e2e5d
szoker <MY_NAME@example.com>
2022-09-22 14:43:59 +0200

    dist

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import 'bootstrap/dist/css/bootstrap.css';
+
+import './main.css'
+
+document.getElementById('uzenet').addEventListener('click', nyeremeny);
+
+function nyeremeny(){
+    document.getElementById('nyere').style.display = "block";
+
+}
